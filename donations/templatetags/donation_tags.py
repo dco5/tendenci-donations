824541044a6f34d8b9e9409284c8c9b746e10436
Max <maxc@bixly.com>
2015-01-27 17:21:17 +0800

    added megemanu for tendenci-donations module T6 version; added translations to templates

--- donations/templatetags/donation_tags.py
+++ donations/templatetags/donation_tags.py
@@ -10,6 +10,14 @@ def donation_nav(context, user, donation=None):
     })
     return context
 
+@register.inclusion_tag("donations/top_nav_items.html", takes_context=True)
+def donation_current_app(context, user, donation=None):
+    context.update({
+        'app_object': donation,
+        "user": user
+    })
+    return context
+
 @register.inclusion_tag("donations/search-form.html", takes_context=True)
 def donation_search(context):
     return context

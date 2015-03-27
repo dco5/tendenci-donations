8796e0d5137e96d91d6f1b6b5cea445660fb2361
Jenny Qian <jqian@tendenci.com>
2015-03-27 16:48:56 -0500

    version bump

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     name='tendenci-donations',
     author='Schipul',
     author_email='programmers@schipul.com',
-    version='6.0.0',
+    version='6.0.1',
     license='GPL3',
     description='Donations addon for Tendenci',
     long_description=longdesc,
